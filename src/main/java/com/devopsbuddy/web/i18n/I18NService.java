e12b6ac93f4965999ea302f8e689094a85e01bdc
taylorjohnt <taylorjohn.9121@gmail.com>
2018-01-29 17:23:59 -0600

    Added login form and login link to the navbar

--- src/main/java/com/devopsbuddy/web/i18n/I18NService.java
+++ src/main/java/com/devopsbuddy/web/i18n/I18NService.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Service;
-import sun.rmi.runtime.Log;
+
 
 import java.util.Locale;
 
